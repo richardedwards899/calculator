a3956e4c17c2ea0467afd7ad166c0eb43d0a9197
Richard Edwards <richardedwards899@gmail.com>
2017-07-07 14:51:08 +0100

    start of exercise

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,21 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('can add two numbers', function(){
+    var expected = 4;
+
+    assert.equal(expected, calculator.);
+  })
+
+
+});
