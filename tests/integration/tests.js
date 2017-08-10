d243952426ac94f1b24d5bf2711356a931789178
Richard Edwards <richardedwards899@gmail.com>
2017-08-10 17:24:26 +0100

    add browser test for 4 * 5 = 20

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -32,13 +32,31 @@ describe('calculator functionality', function() {
   // tests whether the arithmetic operations work to update the display
   // with the result of the operation.
 
-  it('should 2 and 3 to display 5', function(){
+  it('should add 2 and 3 to display 5', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     element(by.css('#operator_add')).click();
     element(by.css('#number3')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('5');
+  })
+
+  it('should subtract 6 from 9 to display 3', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3');
+  })
+
+  it('should multiply 4 by 5 to display 20', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('20');
   })
 
 });
