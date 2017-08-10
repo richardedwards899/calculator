60da4a154692efb39e87a018bb29f21be9b5b64b
Richard Edwards <richardedwards899@gmail.com>
2017-08-10 17:18:35 +0100

    add browser test for 2 plus 3 equals 5

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -9,11 +9,36 @@ describe('calculator functionality', function() {
     browser.get('http://localhost:3000');
   });
 
-  // write integration tests here in the form of "it should do something..."
-  it('should have working number buttons', function(){
+  // tests whether number buttons work to update the running total display
+
+  it('should have working number buttons__0', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number0')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0')
+  })
+
+  it('should have working number buttons__2', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('should have working number buttons__9', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('9')
+  })
+
+  // tests whether the arithmetic operations work to update the display
+  // with the result of the operation.
+
+  it('should 2 and 3 to display 5', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5');
   })
 
 });
