c35531742df33a3883fb505c875aebbad86b69c7
Richard Edwards <richardedwards899@gmail.com>
2017-08-10 17:28:39 +0100

    add browser test for 24 divided by 3 = 8

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -59,4 +59,14 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('20');
   })
 
+  it('should divide 24 by 3 to display 8', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8');
+  })
+
 });
